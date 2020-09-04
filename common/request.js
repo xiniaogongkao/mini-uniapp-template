a162304a4a8b213a34394db7506e57fef3f5b53c
ZhangKai <45391737+zhangkai0621@users.noreply.github.com>
2020-09-04 17:47:06 +0800

    创建模板

---
+++ common/request.js
@@ -0,0 +1,9 @@
+import config from './config.js'
+console.log(config)
+const request = (url = '', params = {}, data = {}, type = 'GET', header = {
+}) => {
+  if (type === 'get')
+}
+export default {
+  
+}
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="container">
+		<view class="intro">通用模板</view>
+		<text class="intro">详见：</text>
+    <u-button type="primary">按钮</u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+    created() {
+      uni.request({
+          url: 'http://apitest.xiniaogongkao.com/appv1/banner', //仅为示例，并非真实接口地址。
+          method: 'post',
+          data: {
+              text: 'uni.request'
+          },
+          header: {
+              'custom-header': 'hello', //自定义请求头信息
+              'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: (res) => {
+              console.log(res.data);
+              this.text = 'request success';
+          }
+      });
+    },
+		methods: {
+      
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 32rpx;
+		line-height: 64rpx;
+	}
+</style>
